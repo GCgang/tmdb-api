b7d13317f955d807a9881e276ce04f857c38c3a3
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-20 03:38:45 +0900

    Feat: Header(react hook form)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,33 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useNavigate, Link } from "react-router-dom";
+interface IFormInput {
+  searchQuery: string;
+}
+
 export default function Header() {
-  return <header></header>;
+  const { register, handleSubmit, reset } = useForm<IFormInput>();
+  const handleClick = () => {
+    return;
+  };
+  const navigate = useNavigate();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    navigate(`/home/${data.searchQuery}`);
+    reset();
+  };
+  return (
+    <header>
+      <div>
+        <Link to={"/"}>
+          <button>Logo</button>
+        </Link>
+        <nav>
+          <Link to="/home">Home</Link>
+        </nav>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="search..." {...register("searchQuery")} />
+        <button onClick={handleClick}>Search</button>
+      </form>
+    </header>
+  );
 }
