c815cf4bb1052bd8aafaf47b981fce83fc0201b2
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-20 01:18:02 +0900

    Feat: React Query Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <QueryClientProvider client={queryClient}>
         <Outlet />
+        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
       <Footer />
     </div>
