498255021dfa0a64b12e42419b9f386d2a146671
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-20 03:38:14 +0900

    Feat: home, search router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/home/:keyword", element: <Home /> },
+    ],
   },
 ]);
