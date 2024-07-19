b06f3a3662c67fed64f8218bb2858dd1fb79ef7b
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-20 01:08:52 +0900

    Feat: layout & react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
