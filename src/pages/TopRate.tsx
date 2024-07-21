bc7bd7d10274ede623770254d816bb10fde181f9
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 03:15:42 +0900

    Feat: TopRated

--- src/pages/TopRate.tsx
+++ src/pages/TopRate.tsx
@@ -1,7 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+import { useTmdbApi } from '../context/TmdbApiContext';
+import { IMovie } from '../api/types';
+import NotFound from './NotFound';
+import MovieDetail from '../components/MovieDetail';
 
 export default function TopRate() {
+  const { tmdb } = useTmdbApi();
+  const {
+    data: topRatedMovies,
+    isLoading,
+    error,
+  } = useQuery<IMovie[]>({
+    queryKey: ['topRated'],
+    queryFn: () => tmdb.topRated(),
+  });
+
+  if (error) return <NotFound />;
   return (
-    <div>
-    </div>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {topRatedMovies?.map((movie) => (
+            <MovieDetail key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
