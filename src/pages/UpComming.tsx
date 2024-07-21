4cbd3517b78ea6e9f53f76ab2796dccbe8f14be0
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 03:16:04 +0900

    Feat: UpComming

--- src/pages/UpComming.tsx
+++ src/pages/UpComming.tsx
@@ -1,3 +1,32 @@
-export default function UpCommming() {
-  return <div></div>;
+import { useQuery } from '@tanstack/react-query';
+import { useTmdbApi } from '../context/TmdbApiContext';
+import { IMovie } from '../api/types';
+import NotFound from './NotFound';
+import MovieDetail from '../components/MovieDetail';
+
+export default function UpComming() {
+  const { tmdb } = useTmdbApi();
+  const {
+    data: upCommingMovies,
+    isLoading,
+    error,
+  } = useQuery<IMovie[]>({
+    queryKey: ['upComming'],
+    queryFn: () => tmdb.upComming(),
+  });
+
+  if (error) return <NotFound />;
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {upCommingMovies?.map((movie) => (
+            <MovieDetail key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
