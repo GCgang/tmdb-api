b64c5cf742017efb2936c1338525032de5da44a2
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 03:15:57 +0900

    Feat: nowPlaying

--- src/pages/NowPlaying.tsx
+++ src/pages/NowPlaying.tsx
@@ -1,3 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+import { useTmdbApi } from '../context/TmdbApiContext';
+import { IMovie } from '../api/types';
+import NotFound from './NotFound';
+import MovieDetail from '../components/MovieDetail';
+
 export default function NowPlaying() {
-  return <div></div>;
+  const { tmdb } = useTmdbApi();
+  const {
+    data: nowPlayingMovies,
+    isLoading,
+    error,
+  } = useQuery<IMovie[]>({
+    queryKey: ['nowPlaying'],
+    queryFn: () => tmdb.NowPlaying(),
+  });
+
+  if (error) return <NotFound />;
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {nowPlayingMovies?.map((movie) => (
+            <MovieDetail key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
