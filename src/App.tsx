10e0b27adcd085235812ea68cb67c9844e4316ba
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 03:21:07 +0900

    Feat: recoil

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { TmdbProvider } from './context/TmdbApiContext';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <div>
-      <Header />
-      <TmdbProvider>
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-          <ReactQueryDevtools initialIsOpen={true} />
-        </QueryClientProvider>
-      </TmdbProvider>
-      <Footer />
+      <RecoilRoot>
+        <Header />
+        <TmdbProvider>
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+            <ReactQueryDevtools initialIsOpen={true} />
+          </QueryClientProvider>
+        </TmdbProvider>
+        <Footer />
+      </RecoilRoot>
     </div>
   );
 }
