301038bbccd2e8ab3290269a6990beef3fd00124
GCgang <powm97@gmail.com>
2024-07-21 14:59:57 +0900

    Feat: logo, search icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+import { RiNetflixFill } from 'react-icons/ri';
+
 interface IFormInput {
   searchQuery: string;
 }
 
 export default function Header() {
   const { register, handleSubmit, reset } = useForm<IFormInput>();
-  const handleClick = () => {
-    return;
-  };
   const navigate = useNavigate();
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     navigate(`/home/${data.searchQuery}`);
@@ -18,15 +18,21 @@ export default function Header() {
     <header>
       <div>
         <Link to={'/'}>
-          <button>Logo</button>
+          <RiNetflixFill />
         </Link>
         <nav>
           <Link to="/home">Home</Link>
+          <Link to="/toprate">TopRate</Link>
+          <Link to="/popular">Popular</Link>
+          <Link to="/upcomming">UpCommming</Link>
+          <Link to="/nowplaying">NowPlaying</Link>
         </nav>
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input placeholder="search..." {...register('searchQuery')} />
-        <button onClick={handleClick}>Search</button>
+        <button>
+          <BsSearch />
+        </button>
       </form>
     </header>
   );
