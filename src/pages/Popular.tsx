e508c5f423601e87defa518a671fc6414c4df256
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 03:15:50 +0900

    Feat: Popular

--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -1,3 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+import { useTmdbApi } from '../context/TmdbApiContext';
+import { IMovie } from '../api/types';
+import NotFound from './NotFound';
+import MovieDetail from '../components/MovieDetail';
+
 export default function Popular() {
-  return <div></div>;
+  const { tmdb } = useTmdbApi();
+  const {
+    data: popularMovies,
+    isLoading,
+    error,
+  } = useQuery<IMovie[]>({
+    queryKey: ['popular'],
+    queryFn: () => tmdb.popular(),
+  });
+
+  if (error) return <NotFound />;
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {popularMovies?.map((movie) => (
+            <MovieDetail key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
