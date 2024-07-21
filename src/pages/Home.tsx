3289f58708c0a28fed173fef35475f4ef3a1b0e8
GCgang <powm97@gmail.com>
2024-07-21 21:09:45 +0900

    Feat: Home-배경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import SearchResults from './SearchResults';
 import { useQuery } from '@tanstack/react-query';
 import { useTmdbApi } from '../context/TmdbApiContext';
+import MovieSlider from '../components/MovieSlider';
 
 interface Backdrop {
   file_path: string;
@@ -34,44 +35,23 @@ export default function Home() {
     error: errorNowPlaying,
   } = useQuery({ queryKey: ['nowPlaying'], queryFn: () => tmdb.NowPlaying() });
 
-  const movieId = topRatedMovies?.[0]?.id;
-
-  const {
-    data: movieImages,
-    isLoading: isLoadingMovieImages,
-    error: errorMovieImages,
-  } = useQuery({
-    queryKey: ['movieImages', movieId],
-    queryFn: () => tmdb.movieImages(movieId),
-    enabled: !!movieId,
-  });
-
-  console.log(movieImages);
   return (
     <div>
       {keyword && keyword.length > 0 ? (
         <SearchResults />
       ) : (
         <>
-          {movieImages &&
-            movieImages.backdrops &&
-            movieImages.backdrops.length > 0 && (
-              <div>
-                {movieImages.backdrops
-                  .slice(0, 5)
-                  .map((image: Backdrop, index: number) => (
-                    <img
-                      key={index}
-                      src={`https://image.tmdb.org/t/p/w1280${image.file_path}`}
-                      alt={`Movie Image ${index + 1}`}
-                    />
-                  ))}
-              </div>
-            )}
-          <h1>{topRatedMovies?.[0]?.title}</h1>
-          <p>{topRatedMovies?.[0]?.overview}</p>
+          {topRatedMovies && topRatedMovies.length > 0 && (
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w1280${topRatedMovies[0].backdrop_path}`}
+                alt={topRatedMovies[0].title}
+              />
+              <h1>{topRatedMovies[0].title}</h1>
+              <p>{topRatedMovies[0].overview}</p>
+            </div>
+          )}
         </>
-        
       )}
     </div>
   );
