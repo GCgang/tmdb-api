7a26b5bfe1c434e8c282b80bce18e357d79611e9
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-22 01:24:52 +0900

    Feat: not found

--- src/pages/TopRate.tsx
+++ src/pages/TopRate.tsx
@@ -1,16 +1,7 @@
-import { Link } from 'react-router-dom';
 
 export default function TopRate() {
   return (
     <div>
-      <h1>길을 잃으셨나요 ?</h1>
-      <p>
-        죄송합니다. 해당 페이지를 찾을 수 없습니다. 홈페이지로 이동해 다양한
-        콘텐츠를 만나보세요.
-      </p>
-      <Link to={'/'}>
-        <button>홈으로</button>
-      </Link>
     </div>
   );
 }
