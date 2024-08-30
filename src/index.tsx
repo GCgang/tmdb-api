4533b9cbb32dddc91656cb85f8bc97d19e37db90
GCgang <powm97@gmail.com>
2024-08-30 20:13:16 +0900

    Feat: GlobalCSS-normalize

--- src/index.tsx
+++ src/index.tsx
@@ -7,74 +7,20 @@ import { theme } from './theme';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import normalize from 'styled-normalize';
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
-  line-height: 1.2;
-  color: ${(props) => props.theme.white.darker};
-  background-color: black;
-}
-a {
-  text-decoration:none;
-  color:inherit;
-}
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}   
+  *{box-sizing:border-box}
+  a{text-decoration:none; color:inherit}
+  ul{list-style:none;padding:0;}
+  body{ font-family: 'Noto Sans KR', sans-serif; color:#fff; background-color: #000; overflow-x:hidden;}
+  button {outline:none;border:0 none; background-color:transparent;cursor: pointer;}
+  h4{margin:0}
 `;
 
 const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(
+const root = ReactDOM.createRoot( 
   document.getElementById('root') as HTMLElement
 );
 root.render(
