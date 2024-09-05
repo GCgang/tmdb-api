8a4c469028efd96db3226da7e76f0c7339b5ea09
GCgang <powm97@gmail.com>
2024-09-05 17:31:48 +0900

    Refactor: footer 하단 고정

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,47 @@ import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 import { TmdbProvider } from './context/TmdbApiContext';
 import MovieModal from './components/Modal/MovieModal';
-import { ToastContainer, Bounce } from 'react-toastify'; // Bounce를 import
+import { ToastContainer, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import styled from 'styled-components';
 function App() {
   return (
-    <>
+    <Wrapper>
       <Header />
-      <TmdbProvider>
-        <Outlet />
-        <MovieModal />
-      </TmdbProvider>
-      <ToastContainer
-        position='top-right'
-        autoClose={1500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='light'
-        transition={Bounce}
-      />
+      <Content>
+        <TmdbProvider>
+          <Outlet />
+          <MovieModal />
+        </TmdbProvider>
+        <ToastContainer
+          position='top-right'
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+          transition={Bounce}
+        />
+      </Content>
       <Footer />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.main`
+  flex: 1;
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 export default App;
